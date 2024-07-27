42349843bca453cf292ecdf0322a0af8b3ad1381
chieh <secret12501250@gmail.com>
2024-07-27 10:31:03 +0800

    chore: 更新 base: '/Repository 的名稱/'

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/2024-hexschool-vite-website-training/",
+  base: "/2024-hexschool-web-layout-training-vite/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
